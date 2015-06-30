ad8d3ff4c8187bd96595a011d7dff9a1bbcd00ea
gumonet <gutierrez.monola@gmail.com>
2015-06-30 18:54:12 -0500

    Establecimiento del Config

--- Mi Negocio/Form1.Designer.cs
+++ Mi Negocio/Form1.Designer.cs
@@ -28,12 +28,75 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(35, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "label1";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(13, 26);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(259, 20);
+            this.textBox1.TabIndex = 1;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(13, 52);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(259, 20);
+            this.textBox2.TabIndex = 2;
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(12, 78);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(259, 20);
+            this.textBox3.TabIndex = 3;
+            // 
+            // textBox4
+            // 
+            this.textBox4.Location = new System.Drawing.Point(13, 104);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(259, 20);
+            this.textBox4.TabIndex = 4;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 261);
+            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.TextBox textBox4;
     }
 }
 
--- Mi Negocio/Form1.cs
+++ Mi Negocio/Form1.cs
@@ -6,6 +6,7 @@ using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Windows.Forms;
+using System.Xml;
 
 namespace Mi_Negocio
 {
@@ -14,6 +15,51 @@ namespace Mi_Negocio
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            label1.Text = AppDomain.CurrentDomain.BaseDirectory;
+            string path = AppDomain.CurrentDomain.BaseDirectory.ToString();
+            XmlDocument xDoc = new XmlDocument();
+            xDoc.Load("config.xml");
+            XmlNode configuracion = xDoc.DocumentElement.SelectSingleNode("/configuracion/database");
+            textBox1.Text = configuracion.Attributes["host"].Value;
+            textBox2.Text = configuracion.Attributes["database"].Value;
+            textBox3.Text = configuracion.Attributes["usuario"].Value;
+            textBox4.Text = configuracion.Attributes["password"].Value;
+
+            /*
+             * XmlDocument doc = new XmlDocument();
+doc.LoadXml(xmlstring);
+
+XmlNode errorNode = doc.DocumentElement.SelectSingleNode("/DataChunk/ResponseChunk/Errors/error");
+
+string errorCode = errorNode.Attributes["code"].Value;
+string errorMessage = errorNode.InnerText;
+             * 
+             * 
+             * XmlNodeList personas = xDoc.GetElementsByTagName("personas");
+XmlNodeList lista = 
+    ((XmlElement)personas[0]).GetElementsByTagName("persona"); 
+foreach (XmlElement nodo in lista)
+{
+int i=0;
+XmlNodeList nNombre = 
+nodo.GetElementsByTagName("nombre");
+
+XmlNodeList nApellido1 = 
+nodo.GetElementsByTagName("apellido1");
+
+XmlNodeList nApellido2 = 
+nodo.GetElementsByTagName("apellido2");
+
+Console.WriteLine("Elemento nombre ... {0} {1} {2}", 
+                             nNombre[i].InnerText, 
+                             nApellido1[i].InnerText,
+                             nApellido2[i++].InnerText);
+
+} */
         }
     }
 }
